55ef64d88a6d14d31d098f9ec5c360124464bbb0
Benjamin Okkema <ben@okkema.org>
2021-03-06 14:53:51 -0600

    inline settings component

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -2,8 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  template: `
+    <mat-card>
+      <div fxLayout="row" fxLayoutAlign="start center">
+          <img mat-card-avatar src="./assets/img/settings.png">
+          <mat-card-title [style.margin-bottom]="0" [style.margin-left.px]="16" >Settings</mat-card-title>
+      </div>
+      <hr>
+      <mat-card-content></mat-card-content>
+    </mat-card>
+  `,
 })
 export class SettingsComponent implements OnInit {
 
--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { SettingsComponent } from './settings.component';
-import { SettingsRoutingModule } from './settings-routing.module';
+import { SettingsRoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
