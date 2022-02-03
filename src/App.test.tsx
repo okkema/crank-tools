8a692ecba71e9f15246b8f1f2c9cd7aa8a63998e
Benjamin Okkema <ben@okkema.org>
2022-02-02 23:32:25 -0600

    add app bar and navigation drawer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
 import App from "./App"
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
+const renderApp = () => render(<App />)
 
 describe("App", () => {
   it("displays the title", () => {
-    render(<App />)
-    expect(screen.getByText(/Crank Tools/i)).toBeInTheDocument()
+    renderApp()
+    expect(screen.getByText(/crank tools/i)).toBeInTheDocument()
+  })
+  it("displays the content", () => {
+    renderApp()
+    expect(screen.getByText(/content/i)).toBeInTheDocument()
+  })
+  it("toggles the navigation drawer", async () => {
+    renderApp()
+    expect(screen.queryByText(/navigation/i)).not.toBeInTheDocument()
+    userEvent.click(screen.getByRole("button", { name: /menu/i }))
+    expect(screen.getByText(/navigation/i)).toBeInTheDocument()
+    userEvent.keyboard("{esc}")
+    await waitFor(() => {
+      expect(screen.queryByText(/navigation/i)).not.toBeInTheDocument()
+    })
   })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { useState } from "react"
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Container,
+  Drawer,
+} from "@mui/material"
+import { Menu } from "@mui/icons-material"
+
 const App = () => {
-  return <div>Crank Tools</div>
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            sx={{ marginRight: 2 }}
+            onClick={() => setOpen(!open)}
+            aria-label="menu"
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" component="div" flexGrow="1">
+            Crank Tools
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
+        Navigation
+      </Drawer>
+      <Container>Content</Container>
+    </>
+  )
 }
 
 export default App
