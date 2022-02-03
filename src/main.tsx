4b99648f919068c6ec0a3053bd57eb59fcfd07e8
Benjamin Okkema <ben@okkema.org>
2022-02-02 22:36:40 -0600

    add css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
+import CssBaseline from "@mui/material/CssBaseline"
 import "@fontsource/roboto/300.css"
 import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
@@ -8,6 +9,7 @@ import "@fontsource/roboto/700.css"
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById("root"),
