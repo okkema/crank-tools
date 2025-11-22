cce0bb48c1d7575f4fb415f7c46159bf04c34c3e
Benjamin Okkema <ben@okkema.org>
2025-11-21 20:57:10 -0600

    feat: add customer schema

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+    dialect: "sqlite",
+    schema: [
+        "./src/schemas/*.ts",
+    ],
+    out: "./migrations",
+});
