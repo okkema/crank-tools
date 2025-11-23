21b0f0e9eb6f34b465e2bd7c88fd7a11643b2bf9
Benjamin Okkema <ben@okkema.org>
2025-11-22 22:04:23 -0600

    feat: add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import { defineConfig, envField } from "astro/config";
+import svelte from "@astrojs/svelte";
+import node from "@astrojs/node";
+
+const adapter = (function(environment){
+  switch (environment) {
+    case "local": 
+      return node({
+        mode: "standalone",
+      });
+    case "demo":
+    default:
+      break;
+  }
+})(process.env.ENVIRONMENT);
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  
+  adapter,
+  env: {
+    schema: {
+      ENVIRONMENT: envField.string({ context: "client", access: "public" })
+    }
+  }
 });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from "drizzle-kit";
 import { LocalDatabase, MigrationsFolder } from "./src/Database";
 
+const [driver, dbCredentials] = (function(environment) {
+    switch (environment) {
+        case "local":
+            return [undefined, { url: LocalDatabase }];
+        case "demo":
+        default:
+            throw new Error(`Unable to process migrations for \"${environment}\"`);
+    }
+})(process.env.ENVIRONMENT);
+
 export default defineConfig({
     dialect: "sqlite",
     schema: [
         "./src/schema.ts",
     ],
     out: MigrationsFolder,
-    dbCredentials: {
-        url: LocalDatabase
-    }
+    driver,
+    dbCredentials,
 });
