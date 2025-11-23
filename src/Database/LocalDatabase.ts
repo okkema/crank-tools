f7dedc11cacd931977621babd4dcaecbcd64d5c8
Benjamin Okkema <ben@okkema.org>
2025-11-22 21:35:02 -0600

    fix: local database url

--- src/Database/LocalDatabase.ts
+++ src/Database/LocalDatabase.ts
@@ -2,9 +2,9 @@ import { createClient } from "@libsql/client";
 import type { Database, DatabaseSchema } from "./DatabaseSchema";
 import { drizzle } from "drizzle-orm/libsql";
 
-export const LocalDatabase = "./database.sqlite"
+export const LocalDatabase = "file:./database.sqlite"
 
-export async function createLocalDatabase<T extends DatabaseSchema>(schema: T, url: string = LocalDatabase): Promise<Database<T>> {
+export async function createLocalDatabase<T extends DatabaseSchema = DatabaseSchema>(schema: T, url: string = LocalDatabase): Promise<Database<T>> {
     const client = createClient({
         url,
     });
