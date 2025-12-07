2b12eaaaabde42a7e9dab603c7f9061172203b1f
Benjamin Okkema <ben@okkema.org>
2025-12-06 21:46:22 -0600

    feat: add form select

---
+++ src/Forms/FormSelect.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Select, { Option } from "@smui/select";
+
+    interface Props {
+        label: string;
+        name: string;
+        value?: string;
+        options: {
+            value: string;
+            label: string;
+        }[];
+    }
+
+    let { label, name, value = $bindable(), options }: Props = $props();
+</script>
+
+<Select bind:value {label} hiddenInput input$name={name} style="width: 100%;">
+    {#each options as option}
+        <Option value={option.value}>{option.label}</Option>
+    {/each}
+</Select>
--- src/Forms/index.ts
+++ src/Forms/index.ts
@@ -1,5 +1,6 @@
 import FormHandler from "./FormHandler.svelte";
 import FormInput from "./FormInput.svelte";
 import FormModal from "./FormModal.svelte";
+import FormSelect from "./FormSelect.svelte";
 
-export { FormHandler, FormInput, FormModal };
+export { FormHandler, FormInput, FormModal, FormSelect };
