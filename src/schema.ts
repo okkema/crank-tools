4e65f69209cfe4a39bc0aa8383e2096c9b0cb9f3
Benjamin Okkema <ben@okkema.org>
2025-12-11 21:14:56 -0600

    feat: add product table and repo

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,10 @@
 import { CustomerTable } from "@/Customers/CustomerSchema";
+import { ProductTable } from "@/Products/ProductSchema";
 import { ServiceRelations, ServiceTable } from "@/Service/ServiceSchema";
 
 export {
     CustomerTable,
+    ProductTable,
     ServiceRelations,
     ServiceTable,
 };
